63a1d119a7c65ad78c5ee8a6ea3306943fda9e74
Brad Wilson <dotnetguy@gmail.com>
2014-09-02 09:06:14 -0700

    Update to 2746, and added collection ordering sample.

---
+++ TestOrderExamples/TestCollectionOrdering/DisplayNameOrderer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+public class DisplayNameOrderer : ITestCollectionOrderer
+{
+    public IEnumerable<ITestCollection> OrderTestCollections(IEnumerable<ITestCollection> testCollections)
+    {
+        return testCollections.OrderBy(collection => collection.DisplayName);
+    }
+}
