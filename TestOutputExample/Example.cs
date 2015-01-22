2f3526a1eebf402066378694709d9f30e5258c43
jnewkirk <jn@tier3.com>
2015-01-22 15:41:58 -0800

    Added Example showing how to do output in a test

---
+++ TestOutputExample/Example.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+public class Example
+{
+    ITestOutputHelper output;
+
+    public Example(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
+    [Fact]
+    public void TestThis()
+    {
+        output.WriteLine("I'm inside the test!");
+    }
+}
