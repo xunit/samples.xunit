f033f345d86419a1e3dc0f13772c49625fc71dfb
Andrew Arnott <andarno@microsoft.com>
2015-04-07 08:15:27 -0700

    Fix STAFact sample to stay on STA after await
    Without a SynchronizationContext applied to the STA, an async test method would resume on another MTA thread after any yielding await. This does not reflect the behavior of real world STA threads that appear in GUI apps.
    With this change, async test methods stay on the STA.
    The two recently enhanced tests now pass.

--- STAExamples/STATestCase.cs
+++ STAExamples/STATestCase.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
@@ -40,9 +41,31 @@ namespace STAExamples
             {
                 try
                 {
-                    var testCaseTask = testCase.RunAsync(diagnosticMessageSink, messageBus, constructorArguments, aggregator,
-                        cancellationTokenSource);
-                    tcs.SetResult(testCaseTask.Result);
+                    // Set up the SynchronizationContext so that any awaits
+                    // resume on the STA thread as they would in a GUI app.
+                    SynchronizationContext.SetSynchronizationContext(new DispatcherSynchronizationContext());
+
+                    // Start off the test method.
+                    var testCaseTask = this.testCase.RunAsync(diagnosticMessageSink, messageBus, constructorArguments, aggregator, cancellationTokenSource);
+
+                    // Arrange to pump messages to execute any async work associated with the test.
+                    var frame = new DispatcherFrame();
+                    Task.Run(async delegate
+                    {
+                        try
+                        {
+                            await testCaseTask;
+                        }
+                        finally
+                        {
+                            // The test case's execution is done. Terminate the message pump.
+                            frame.Continue = false;
+                        }
+                    });
+                    Dispatcher.PushFrame(frame);
+
+                    // Report the result back to the Task we returned earlier.
+                    CopyTaskResultFrom(tcs, testCaseTask);
                 }
                 catch (Exception e)
                 {
@@ -98,6 +121,37 @@ namespace STAExamples
         public void Serialize(IXunitSerializationInfo info)
         {
             info.AddValue("InnerTestCase", testCase);
+        }
+
+        private static void CopyTaskResultFrom<T>(TaskCompletionSource<T> tcs, Task<T> template)
+        {
+            if (tcs == null)
+            {
+                throw new ArgumentNullException("tcs");
+            }
+
+            if (template == null)
+            {
+                throw new ArgumentNullException("template");
+            }
+
+            if (!template.IsCompleted)
+            {
+                throw new ArgumentException("Task must be completed first.", "template");
+            }
+
+            if (template.IsFaulted)
+            {
+                tcs.SetException(template.Exception);
+            }
+            else if (template.IsCanceled)
+            {
+                tcs.SetCanceled();
+            }
+            else
+            {
+                tcs.SetResult(template.Result);
+            }
         }
     }
 }
