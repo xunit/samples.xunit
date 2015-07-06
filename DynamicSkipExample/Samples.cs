fa2bb2147dbf3a49db86371f21094561b8ad2083
Brad Wilson <dotnetguy@gmail.com>
2015-07-06 13:19:05 -0700

    Update DynamicSkipExample to support theories, too

--- DynamicSkipExample/Samples.cs
+++ DynamicSkipExample/Samples.cs
@@ -4,24 +4,44 @@ namespace DynamicSkipExample
 {
     public class Samples
     {
-        [SkippableFact]
-        public void Passing() { }
-
-        [SkippableFact(Skip = "I never feel like it")]
-        public void StaticallySkipped() { }
-
-        [SkippableFact]
-        public void DynamicallySkipped()
+        public class Facts
         {
-            // You could hide this behind something like "Assert.Skip", by bring in the assertion
-            // library as source and extending the Assert class.
-            throw new SkipTestException("I don't feel like it right now, ask again later");
+            [SkippableFact]
+            public void Passing() { }
+
+            [SkippableFact(Skip = "I never feel like it")]
+            public void StaticallySkipped() { }
+
+            [SkippableFact]
+            public void DynamicallySkipped()
+            {
+                // You could hide this behind something like "Assert.Skip", by bring in the assertion
+                // library as source and extending the Assert class.
+                throw new SkipTestException("I don't feel like it right now, ask again later");
+            }
+
+            [SkippableFact]
+            public void Failing()
+            {
+                Assert.True(false);
+            }
         }
 
-        [SkippableFact]
-        public void Failing()
+        public class Theories
         {
-            Assert.True(false);
+            [SkippableTheory(Skip = "I never feel like it")]
+            public void StaticallySkipped() { }
+
+            [SkippableTheory]
+            [InlineData(null)]
+            [InlineData("Hello, world!")]
+            public void DynamicallySkipped(string value)
+            {
+                if (value == null)
+                    throw new SkipTestException("I don't like to run against null data.");
+
+                Assert.StartsWith("foo", value);
+            }
         }
     }
 }
---
+++ DynamicSkipExample/SkippableTheoryAttribute.cs
@@ -0,0 +1,8 @@
+﻿using Xunit;
+using Xunit.Sdk;
+
+namespace DynamicSkipExample
+{
+    [XunitTestCaseDiscoverer("DynamicSkipExample.XunitExtensions.SkippableTheoryDiscoverer", "DynamicSkipExample")]
+    public class SkippableTheoryAttribute : TheoryAttribute { }
+}
