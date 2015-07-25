969d9f7e887836f01a6c525324bf3db55658c28f
Brad Wilson <dotnetguy@gmail.com>
2015-07-25 10:51:09 -0700

    Move `[WpfFact]` out of the namespace (to match `[WpfTheory]`)

--- STAExamples/WpfFactAttribute.cs
+++ STAExamples/WpfFactAttribute.cs
@@ -2,9 +2,6 @@
 using Xunit;
 using Xunit.Sdk;
 
-namespace STAExamples
-{
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    [XunitTestCaseDiscoverer("STAExamples.WpfFactDiscoverer", "STAExamples")]
-    public class WpfFactAttribute : FactAttribute { }
-}
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+[XunitTestCaseDiscoverer("STAExamples.WpfFactDiscoverer", "STAExamples")]
+public class WpfFactAttribute : FactAttribute { }
