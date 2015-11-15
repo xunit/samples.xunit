c402914875c9a7da2a68c4228ebcb37a5c2dbfc3
Brad Wilson <dotnetguy@gmail.com>
2015-11-14 16:23:04 -0800

    Update to latest build, and convert DynamicSkipExample into a PCL

--- DynamicSkipExample/Properties/AssemblyInfo.cs
+++ DynamicSkipExample/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -18,9 +17,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b2adf4bf-4e46-4ba0-8981-bda53c6a5f89")]
 
 // Version information for an assembly consists of the following four values:
 //
