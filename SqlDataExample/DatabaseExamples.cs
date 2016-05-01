d7e1c64583926e8c90af4ae9f7e96e9dc4519799
Jim Culverwell <jculverwell@gmail.com>
2016-04-30 22:01:27 -0700

    Upgraded OleDbDataAttribute and SqlServerDataAttribute  and added Database Examples project to Samples.sln
    Upgraded OleDbDataAttribute and SqlServerDataAttribute to work with the
    latest version of xUnit.  Originally found in xUnit Extensions here
    https://github.com/abclassic/Xunit/tree/master/src/xunit.extensions/DataTheories.
    Also added Database example project to showcase their use.

---
+++ SqlDataExample/DatabaseExamples.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+
+public class DatabaseExamples
+{
+    /// <summary>
+    /// Connects to a SQL Server database called TestDatabase (You will need to pass in your own database name)
+    /// </summary>
+    /// <param name="FirstName"></param>
+    /// <param name="LastName"></param>
+    [Theory]
+    [SqlServerData("(local)", "TestDatabase", "select FirstName, LastName from Users")]
+    public void SqlServerTests(string FirstName, string LastName)
+    {
+        Assert.Equal("Peter Beardsley", String.Format("{0} {1}", FirstName, LastName));
+    }
+
+    /// <summary>
+    /// Connects to an OleDB datasource using a connectionstring
+    /// </summary>
+    /// <param name="FirstName"></param>
+    /// <param name="LastName"></param>
+    [Theory]
+    [OleDbData(@"Provider=SQLOLEDB;Server=(local);Database=TestDatabase;Trusted_Connection=yes;", "select FirstName, LastName from Users")]
+    public void OleDbTests(string FirstName, string LastName)
+    {
+        Assert.Equal("Peter Beardsley", String.Format("{0} {1}", FirstName, LastName));
+    }
+}
