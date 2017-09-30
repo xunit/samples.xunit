9a46c2ac81dd4d8e600a1b085b2b18bc3d8f855d
Brad Wilson <dotnetguy@gmail.com>
2017-09-30 13:41:31 -0700

    Update SqlDataExample so that it doesn't try to enumerate data during discovery by default

--- SqlDataExample/DataAdapterDataAttribute.cs
+++ SqlDataExample/DataAdapterDataAttribute.cs
@@ -2,21 +2,27 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Reflection;
-using Xunit;
 using Xunit.Sdk;
-
 
 /// <summary>
 /// Represents an implementation of <see cref="DataAttribute"/> which uses an
 /// instance of <see cref="IDataAdapter"/> to get the data for a <see cref="TheoryAttribute"/>
 /// decorated test method.
 /// </summary>
+[DataDiscoverer("DataAdapterDataAttributeDiscoverer", "SqlDataExample")]
 public abstract class DataAdapterDataAttribute : DataAttribute
 {
     /// <summary>
     /// Gets the data adapter to be used to retrieve the test data.
     /// </summary>
     protected abstract IDataAdapter DataAdapter { get; }
+
+    /// <summary>
+    /// Returns <c>true</c> if the data attribute wants to enumerate data during discovery. By
+    /// default, this attribute assumes that data enumeration is too expensive to do during
+    /// discovery, and therefore defaults to <c>false</c>.
+    /// </summary>
+    public bool EnableDiscoveryEnumeration { get; set; }
 
     /// <inheritdoc/>
     public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest)
---
+++ SqlDataExample/DataAdapterDataAttributeDiscoverer.cs
@@ -0,0 +1,8 @@
+﻿using Xunit.Abstractions;
+using Xunit.Sdk;
+
+public class DataAdapterDataAttributeDiscoverer : DataDiscoverer
+{
+    public override bool SupportsDiscoveryEnumeration(IAttributeInfo dataAttribute, IMethodInfo testMethod)
+        => dataAttribute.GetNamedArgument<bool>("EnableDiscoveryEnumeration");
+}
