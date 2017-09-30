363d5c1dfe704b0c1a3be39bda48c64a1026ab2a
Brad Wilson <dotnetguy@gmail.com>
2017-09-30 12:14:42 -0700

    Convert to SDK-style .csproj files

--- GlobalAssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
-
-[assembly: AssemblyCompany(".NET Foundation")]
-[assembly: AssemblyProduct("xUnit.net Testing Framework")]
-[assembly: AssemblyCopyright("Copyright (C) .NET Foundation")]
-[assembly: AssemblyVersion("2.0.0.0")]
