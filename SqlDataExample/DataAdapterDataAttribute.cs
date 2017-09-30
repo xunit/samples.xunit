58326c42a595ef9eebebb54702b313d08055cd6b
Jim Culverwell <jculverwell@gmail.com>
2017-10-01 05:25:40 +1100

    Upgraded OleDbDataAttribute and SqlServerDataAttribute  and added Database Examples project to Samples.sln (#17)
    Upgraded OleDbDataAttribute and SqlServerDataAttribute to work with the
    latest version of xUnit.  Originally found in xUnit Extensions here
    https://github.com/abclassic/Xunit/tree/master/src/xunit.extensions/DataTheories.
    Also added Database example project to showcase their use.

---
+++ SqlDataExample/DataAdapterDataAttribute.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using Xunit;
+using Xunit.Sdk;
+
+
+/// <summary>
+/// Represents an implementation of <see cref="DataAttribute"/> which uses an
+/// instance of <see cref="IDataAdapter"/> to get the data for a <see cref="TheoryAttribute"/>
+/// decorated test method.
+/// </summary>
+public abstract class DataAdapterDataAttribute : DataAttribute
+{
+    /// <summary>
+    /// Gets the data adapter to be used to retrieve the test data.
+    /// </summary>
+    protected abstract IDataAdapter DataAdapter { get; }
+
+    /// <inheritdoc/>
+    public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest)
+    {
+        DataSet dataSet = new DataSet();
+        IDataAdapter adapter = DataAdapter;
+
+        try
+        {
+            adapter.Fill(dataSet);
+
+            foreach (DataRow row in dataSet.Tables[0].Rows)
+                yield return ConvertParameters(row.ItemArray);
+        }
+        finally
+        {
+            IDisposable disposable = adapter as IDisposable;
+            if (disposable != null)
+                disposable.Dispose();
+        }
+    }
+
+    object[] ConvertParameters(object[] values)
+    {
+        object[] result = new object[values.Length];
+
+        for (int idx = 0; idx < values.Length; idx++)
+            result[idx] = ConvertParameter(values[idx]);
+
+        return result;
+    }
+
+    /// <summary>
+    /// Converts a parameter to its destination parameter type, if necessary.
+    /// </summary>
+    /// <param name="parameter">The parameter value</param>
+    /// <returns>The converted parameter value</returns>
+    protected virtual object ConvertParameter(object parameter)
+    {
+        if (parameter is DBNull)
+            return null;
+
+        return parameter;
+    }
+}
