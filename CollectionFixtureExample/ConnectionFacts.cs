0c8ef4e4291b0ddc98469ad1317d3ad0c7904ce7
Brad Wilson <dotnetguy@gmail.com>
2017-09-30 14:25:08 -0700

    Fix CollectionFixtureExample to run against LocalDB from SQL Express 2016

--- CollectionFixtureExample/ConnectionFacts.cs
+++ CollectionFixtureExample/ConnectionFacts.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
+// Can't get these tests to run? Make sure you've installed LocalDB, a feature in the (free) SQL Server Express.
+// Note that you don't need the "instance" server of SQL Express, just the LocalDB feature.
+
 [Collection("DatabaseCollection")]
 public class ConnectionTests 
 {
--- CollectionFixtureExample/InsertTests.cs
+++ CollectionFixtureExample/InsertTests.cs
@@ -2,6 +2,9 @@
 using System.Data.SqlClient;
 using Xunit;
 
+// Can't get these tests to run? Make sure you've installed LocalDB, a feature in the (free) SQL Server Express.
+// Note that you don't need the "instance" server of SQL Express, just the LocalDB feature.
+
 [Collection("DatabaseCollection")]
 public class InsertTests
 {
