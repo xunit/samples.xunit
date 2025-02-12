21c8d5fed3dd5cf722c1e5e765afa69bc7bf533c
Brad Wilson <dotnetguy@gmail.com>
2025-02-12 12:26:23 -0800

    Incomplete comment

--- v3/AssertExtensions/ExtendingAssert/PersonAssertions.cs
+++ v3/AssertExtensions/ExtendingAssert/PersonAssertions.cs
@@ -1,6 +1,7 @@
 namespace Xunit;
 
-// When importing xUnit.net's assertion library as source, the Assert class is partial and you can additional
+// When importing xUnit.net's assertion library as source, the Assert class is partial and you can add
+// additional methods directly to Assert.
 public partial class Assert
 {
     public static void IsBrad(Person person) =>
